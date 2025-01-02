8da48c98a705fea8f54a71ca006ff51243465014
unknown <s.r.mahamid@gmail.com>
2025-01-02 22:35:09 +0500

    448. Find All Numbers Disappeared in an Array Done

--- Easy/448. Find All Numbers Disappeared in an Array/CODE.cpp
+++ Easy/448. Find All Numbers Disappeared in an Array/CODE.cpp
@@ -1,24 +1,21 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> findDisappearedNumbers(vector<int> &nums)
-    {
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> result;
-        unordered_map<int, int> hashMap;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (hashMap.find(nums[i]) == hashMap.end())
-            {
-                hashMap[nums[i]] = 1;
+
+        // Mark the indices corresponding to the numbers in the array
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1; // Use absolute value to handle already-negated numbers
+            nums[index] = -abs(nums[index]); // Negate the value at the index
+        }
+
+        // Find the indices that were not marked
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                result.push_back(i + 1); // The number corresponding to this index is missing
             }
         }
-        for (int i = 1; i <= nums.size(); i++)
-        {
-            if (hashMap.find(i) == hashMap.end())
-            {
-                result.push_back(i);
-            }
-        }
+
         return result;
     }
 };
